8c473f6f66d20e0fd11a70ae191dc19386dcb271
nethararathnaweera <nethararathnaweera2003@gmail.com>
2026-05-17 15:33:34 +0530

    1st commit

---
+++ appointment/src/main/java/com/example/appointment/entity/Appointment.java
@@ -0,0 +1,4 @@
+package com.example.appointment.entity;
+
+public class Appointment {
+}
---
+++ appointment/src/main/java/com/example/appointment/entity/AppointmentStatus.java
@@ -0,0 +1,9 @@
+package com.example.appointment.entity;
+
+public enum AppointmentStatus   {
+    PENDING,
+    CONFIRMED,
+    COMPLETED,
+    CANCELLED
+
+}
