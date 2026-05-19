6a44b2524ca977f4336e1558932609f3bc93db4f
Nethara Rathnaweera <nethararathnaweera2003@gmail.com>
2026-05-19 21:09:39 +0530

    3rd commit - Add OnlineAppointment subclass - inheritance

---
+++ appointment/src/main/java/com/example/appointment/entity/OnlineAppointment.java
@@ -0,0 +1,28 @@
+package com.example.appointment.entity;
+
+public class OnlineAppointment extends Appointment {
+
+
+    private String platform;
+
+
+    public OnlineAppointment(int appointmentID, int customerID,
+                             String customerName, String date,
+                             String time, String platform) {
+        super(appointmentID, customerID, customerName, date, time);
+        this.platform = platform;
+    }
+
+    public OnlineAppointment() {}
+
+    
+    public String displayInfo() {
+        return super.displayInfo()
+                + " | Booked via: " + platform;
+    }
+
+    public String getPlatform()          {
+        return platform; }
+    public void setPlatform(String p)    {
+        this.platform = p; }
+}
