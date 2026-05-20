a47b282b9f1008cc8a04633d030bb5822ca2ba8d
IT25102184 <it25102184@my.sliit.lk>
2026-05-20 14:11:04 +0530

    - Added model classes (HairService, SkinService, Service) - Added ServiceController - Added ServiceFileRepository

---
+++ src/main/java/com/beautysalon/beautysalonbooking/BeautySalonBookingApplication.java
@@ -0,0 +1,13 @@
+package com.beautysalon.beautysalonbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalonBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautySalonBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beautysalon/beautysalonbooking/BeautySalonBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beautysalon.beautysalonbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySalonBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
